19e86567d6d821e9aa7063bfd1218a5f6bc4644f
Laokeng Vang <laokengv@gmail.com>
2023-05-30 20:58:23 -0500

    Got through to stretch goal +5 and -5 buttons with Michael Duren

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,15 +3,22 @@ import { useState } from 'react';
 export function Counter(props) {
 	const [count, setCount] = useState(0)
 
-	const increment = () => {
-		setCount(count + 1);
+	const increment = (num) => {
+		setCount(count + num);
+	}
+
+	const decrement = (num) => {
+		setCount(count - num)
 	}
 
 	return (
 		<div>
-            <h3>{ props.label } Counter</h3>
+      <h3>{ props.label } Counter</h3>
 			<span>I have been clicked {count} times!</span>
-			<button onClick={increment}>Increment</button>
+			<button onClick={() => increment(1)}>Increment</button>
+			<button onClick={() => decrement(1)}>Decrement</button>
+            <button onClick={() => increment(5)}>+5</button>
+            <button onClick={() => decrement(5)}>-5</button>
 		</div>
 	)
 }
